6dfc0e0b08ee5fade877ee41ccd7b17c75321b04
Artos Web <nokian7733@abv.bg>
2022-01-25 19:14:37 +0200

    new exercises

---
+++ 9. DOM Introduction/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let textElement = document.getElementById('text');
+    textElement.style.display = 'inline';
+    let readMoreElement = document.getElementById('more');
+    readMoreElement.style.display = 'none';
+}
